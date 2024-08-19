6919ff58e5f55166004f77837b57a8554d24a1a0
Daniel Ceballos <danielceballos94@gmail.com>
2024-08-19 01:15:26 -0400

    create basic server, model

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/videoDatabase");
+    console.log("Mongo DB connected");
+  } catch (error) {
+    console.log("Error connecting to db");
+    process.exit(1);
+  }
+}
+
+connectToDatabase();
+
+const videoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  category: String,
+  uploaded_by: String,
+  upload_date: Date,
+  thumbnail_url: String,
+  video_id: String,
+});
+
+const Video = mongoose.model("Video", videoSchema);
+
+app.get("/metadata/videos", async (req: Request, res: Response) => {
+  try {
+    const videos = await Video.find().limit(15);
+    res.json(videos);
+  } catch (error) {
+    console.log("error fetching videos", error);
+    res.status(500).json({ error: "failed to fetch videos" });
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
